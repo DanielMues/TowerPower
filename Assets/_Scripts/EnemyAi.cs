256f49b9a6e904c01886654fd9764039d3ac84aa
DanielMues <54837088+DanielMues@users.noreply.github.com>
2022-07-07 20:38:57 +0200

    update enemy ai

--- Assets/_Scripts/EnemyAi.cs
+++ Assets/_Scripts/EnemyAi.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class EnemyAi : MonoBehaviour
 {
-    public GameObject player;
     private CustomDataStorage customData;
     // Start is called before the first frame update
     private Vector3 gridOriginPosition;
     private Grid playerGrid;
     private WaveHandler waveHandler;
     void Start() {
-        playerGrid = player.GetComponent<GridInit>().getGrid();
-        waveHandler = player.GetComponent<WaveHandler>();
+        playerGrid = this.GetComponent<GridInit>().getGrid();
+        waveHandler = this.GetComponent<WaveHandler>();
     }
 
     // Update is called once per frame
@@ -20,7 +19,72 @@ public class EnemyAi : MonoBehaviour
     {
         if(waveHandler.GetCurrentRunnerAmount() < waveHandler.GetRunnerListMax())
         {
-            waveHandler.addRunner(waveHandler.enemy[0].name);
+            addRunners();
+        }
+    }
+
+    private void addRunners()
+    {
+        float maxTank = 0.4f;
+        float maxSpeedster = 0.4f;
+        float maxAllrounder = 0.5f;
+
+        GameObject tank = null;
+        GameObject allrounder = null;
+        GameObject speedster = null;
+
+        foreach (GameObject runner in waveHandler.enemy)
+        {
+            if (runner.name == "Allrounder")
+            {
+                allrounder = runner;
+            }
+            else if (runner.name == "Speedster")
+            {
+                speedster = runner;
+            }
+            else if (runner.name == "Tank")
+            {
+                tank = runner;
+            }
+        }
+
+        int tankAmount = (int)(waveHandler.GetRunnerListMax() / tank.GetComponent<UnitStats>().spaceTaken * maxTank);
+        int speedsterAmount = (int)(waveHandler.GetRunnerListMax() / speedster.GetComponent<UnitStats>().spaceTaken * maxSpeedster);
+        int allrounderAmount = (int)(waveHandler.GetRunnerListMax() / allrounder.GetComponent<UnitStats>().spaceTaken * maxAllrounder);
+
+        for (int x = 0; x < tankAmount; x++)
+        {
+            waveHandler.addRunner(tank.name);
+        }
+
+        for (int x = 0; x < speedsterAmount; x++)
+        {
+            waveHandler.addRunner(speedster.name);
+        }
+
+        for (int x = 0; x < allrounderAmount; x++)
+        {
+            waveHandler.addRunner(allrounder.name);
+        }
+
+        if (waveHandler.GetCurrentRunnerAmount() < waveHandler.GetRunnerListMax())
+        {
+            int difference = waveHandler.GetRunnerListMax() - waveHandler.GetCurrentRunnerAmount();
+            if (difference % allrounder.GetComponent<UnitStats>().spaceTaken == 0)
+            {
+                while (waveHandler.GetCurrentRunnerAmount() < waveHandler.GetRunnerListMax())
+                {
+                    waveHandler.addRunner(allrounder.name);
+                }
+            }
+            else if (difference % speedster.GetComponent<UnitStats>().spaceTaken == 0)
+            {
+                while (waveHandler.GetCurrentRunnerAmount() < waveHandler.GetRunnerListMax())
+                {
+                    waveHandler.addRunner(speedster.name);
+                }
+            }
         }
     }
 }
